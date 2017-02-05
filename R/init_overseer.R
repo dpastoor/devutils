b606b430906a5d705c4f81fb2779726a2f42d4bb
Devin Pastoor <devin.pastoor@gmail.com>
2017-02-05 11:44:29 -0500

    ability to specify the model to check for interactive model check, safety checks, and documentation

--- R/init_overseer.R
+++ R/init_overseer.R
@@ -1,8 +1,20 @@
 #' initialize an overseer modeling folder
+#' @param check_file file present in overseer directory to confirm proper working directory
 #' @param dir directory name
 #' @param abs whether the directory is
+#' @param overwrite whether to overwrite models.R file if it already exists, defaults to FALSE
+#' @examples \dontrun{
+#' init_overseer() # default to models directory with models.R file inside
+#' init_overseer("vancomcyin_poppk.cpp") # check for cpp file vancomycin_poppk.cpp
+#' init_overseer(dir = "mrgsolve_models") # create directory mrgsolve_models instead of models
+#' }
 #' @export
-init_overseer <- function(dir = "modeling", abs = FALSE) {
+init_overseer <- function(
+                          check_file = NULL,
+                          dir = "modeling",
+                          abs = FALSE,
+                          overwrite = FALSE
+                          ) {
   if (abs) {
     path <- normalizePath(dir, mustWork = FALSE)
   } else {
@@ -12,12 +24,23 @@ init_overseer <- function(dir = "modeling", abs = FALSE) {
     message("creating dir: ", path)
     dir.create(path)
   }
+
+  imc <- paste0("if (!interactive_model_check(\"",
+                ifelse(is.null(check_file),
+                      "<REPLACEWITHYOURFIRSTMODEL.cpp>",
+                       check_file),
+                "\")) {")
+
   models_path <- normalizePath(file.path(path, "models.R"), mustWork = FALSE)
+  if (file.exists(models_path) && !overwrite) {
+    warning("detected models.R file present already, no action taken")
+    return(invisible())
+  }
   message("creating models.R")
   writeLines(c("library(overseer)",
                "",
                "# check to make sure sourcing from proper directory if running interactively",
-               "if (!interactive_model_check(\"<REPLACEWITHYOURFIRSTMODEL.cpp>\")) {",
+               imc,
                "    stop(\"make sure the directory is set to the models directory before running interactively,",
                "    to make sure the relative paths will be the same as when sourcing\")",
                "}",
