a43c423d60b98b65befe0386fe7b9915323d0762
Devin Pastoor <devin.pastoor@gmail.com>
2017-05-26 09:41:08 -0400

    refactor out core functionality to seperate function so can more easily test

--- R/use_bookdown.R
+++ R/use_bookdown.R
@@ -4,9 +4,25 @@
 #' @param ... optional values to pass to infuser
 #' @param .path path to place bookdown template
 #' @param .see_opts just print out the optional values to infuse
-#' @importFrom purrr map map_lgl
+#' @importFrom purrr map map_lgl map2
 use_bookdown <- function(author, description, ..., .path = ".", .see_opts = FALSE) {
   files <- list.files(system.file("bookdown_templates/simple", package = "devutils"), full.names = T)
+  outputs <- infuse_files(files, author = author, description = description, .return_opts = .see_opts)
+  if(.see_opts) {
+    print(outputs)
+    return(outputs)
+  }
+  map2(outputs, files, function(.x, .y) {
+    readr::write_file(.x, file.path(normalizePath(.path), basename(.y)))
+  })
+}
+
+#' infuse files or get the parameters needed
+#' @param files vector of files to infuse
+#' @param ... parameters to pass to infuse
+#' @param .return_opts return the requested variables instead of infusing
+#' @return list
+infuse_files <- function(files, ..., .return_opts = F) {
   vars_requested <- map(files, ~ {
     vars <- infuser::variables_requested(.x)
     if (length(vars)) {
@@ -16,16 +32,12 @@ use_bookdown <- function(author, description, ..., .path = ".", .see_opts = FALS
   }) %>% setNames(basename(files))
   vars_requested <- vars_requested[!map_lgl(vars_requested, is.null)]
 
-  if (.see_opts) {
-    print(vars_requested)
-    return(invisible())
+  if (.return_opts) {
+    return(vars_requested)
   }
 
   outputs <- map(files, function(.x) {
-    infuser::infuse(.x, author = author, description = description)
-  })
-
-  map2(outputs, files, function(.x, .y) {
-    readr::write_file(.x, file.path(normalizePath(.path), basename(.y)))
-  })
+    infuser::infuse(.x, ...)
+  }) %>% setNames(basename(files))
+  return(outputs)
 }
