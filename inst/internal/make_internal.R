3ff5dce547c367bc880f599141732aa7125695de
Devin Pastoor <devin.pastoor@gmail.com>
2017-11-18 14:43:57 -0500

    internal package structure

---
+++ inst/internal/make_internal.R
@@ -0,0 +1,35 @@
+library(magrittr)
+library(remake) # so packrat will grab if using packrat
+
+doc_package <- function(.pkg = "internal") {
+  devtools::document(glue::glue("{.pkg}"))
+  # kick so every time doc'd will have different output
+  Sys.time()
+}
+
+parse_build_message <- function(.x) {
+  .x <- .x[grepl(pattern = "* building", .x)]
+  if (!length(.x)) {
+    stop("error parsing build message - perhaps didn't properly")
+  }
+  .x <- stringr::str_replace(.x, "\\* building ", "")
+  .x <- stringr::str_replace_all(.x, "'", "")
+  return(.x)
+}
+
+build_package <- function(.pkg = "internal") {
+  list(pkg_name = pkgbuild::build(.pkg), time = Sys.time())
+}
+
+check_desc <- function(.f) {
+  file.exists(.f)
+}
+
+#' @param .pkg package details from build_package
+#' @details
+#' expects `pkg_name` and `time` for the build
+install_bin <- function(.pkg_details) {
+  callr::rcmd("INSTALL", c("--library=bin", .pkg_details$pkg_name))
+  message(sprintf("latest package version installed: %s", basename(stringr::str_replace(.pkg_details$pkg_name, ".tar.gz", ""))))
+  Sys.time()
+}
