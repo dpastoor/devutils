e0d849ab81da8b2f31c6ac3f17038535b438b181
Devin Pastoor <devin.pastoor@gmail.com>
2017-11-19 09:39:55 -0500

    add some utility functions

--- R/utils.R
+++ R/utils.R
@@ -7,3 +7,30 @@
 #' @importFrom magrittr %>%
 #' @usage lhs \%>\% rhs
 NULL
+
+
+#' detect if a filepath is for a directory
+#' @param x vector of file paths
+#' @export
+is_dir <- function(x) {
+  isTRUE(file.info(x)$isdir)
+}
+
+#' get the basename of a filepath, minus any extensions
+#' @param .x filepath
+#' @export
+#' @rdname basename_sans_ext
+#' @importFrom tools file_path_sans_ext
+basename_sans_ext <- function(.x) {
+  basename(file_path_sans_ext(.x))
+}
+
+#' recursively create a dir
+#' @param .dir path
+#' @param verbose give a message about dir creation
+mkdirp <- function(.dir, verbose = FALSE) {
+  if (!dir.exists(.dir)) {
+    "!DEBUG creating dir at `.dir`"
+    dir.create(.dir, recursive = TRUE)
+  }
+}
