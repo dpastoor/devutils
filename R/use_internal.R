12867e023a9af034e5a5ad92ba9ce1a8d06b4e15
Devin Pastoor <devin.pastoor@gmail.com>
2017-11-19 09:40:24 -0500

    prelim use_internal implementation

---
+++ R/use_internal.R
@@ -0,0 +1,45 @@
+#' use internal packages setup in a projet
+#' @param .dir directory for internal project packages
+#' @importFrom purrr discard
+#' @export
+use_internal <- function(proj,
+                         first_name,
+                         last_name,
+                         email,
+                         pkg_dir = "./packages") {
+  required_packages <- c(
+    "remake",
+    "glue",
+    "stringr",
+    "pkgbuild"
+  )
+  has_required <- purrr::map_lgl(required_packages,
+                                 requireNamespace,
+                                 quietly = TRUE)
+  if (!all(has_required)) {
+    stop(glue::glue("need to install the following packages: {pkgs}",
+                    pkgs = paste0(required_packages[!has_required],
+                                  collapse = ", ")))
+  }
+  mkdirp(pkg_dir)
+  internal_dir <- system.file("internal",
+             package = "devutils")
+  full_paths <- dir(internal_dir,
+                    full.names = TRUE,
+                    recursive = TRUE) %>%
+    discard(is_dir)
+  relative_paths <- dir(internal_dir,
+                        recursive = TRUE) %>%
+    discard(is_dir)
+
+  folders <- unique(dirname(relative_paths))
+  folders <- folders[folders != "."]
+  map(file.path(pkg_dir, folders), mkdirp)
+  file.copy(from = full_paths,
+            to = file.path(pkg_dir, relative_paths)
+            )
+
+  d <- create_internal_desc(proj, first_name, last_name, email)
+  d$write(file.path(pkg_dir, "internal", "DESCRIPTION"))
+
+}
