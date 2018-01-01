5ff753f5592d87a506e25af5ebcf5a122754ff1a
Devin Pastoor <devin.pastoor@gmail.com>
2018-01-01 13:29:48 -0500

    reload internal functionality

---
+++ R/reload.R
@@ -0,0 +1,19 @@
+#' reload a package namespace
+#' @param pkg package to reload
+#' @param ... arguements to pass to [library()]
+#' @details
+#' used to unload and reload a package namespace,
+#' can be helpful when a package has been rebuilt in a
+#' separate process
+#' @export
+reload_namespace <- function(pkg, ...) {
+  unloadNamespace(pkg)
+  library(pkg, character.only = TRUE, ...)
+}
+
+
+#' @rdname reload_namespace
+#' @export
+reload_internal <- function() {
+  reload_namespace("internal")
+}
