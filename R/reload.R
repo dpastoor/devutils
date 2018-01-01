f5a7688d169575c806581ea1d79ff0aac14ad5f9
Devin Pastoor <devin.pastoor@gmail.com>
2018-01-01 14:24:33 -0500

    separate Rdocs

--- R/reload.R
+++ R/reload.R
@@ -1,6 +1,6 @@
 #' reload a package namespace
 #' @param pkg package to reload
-#' @param ... arguements to pass to [library()]
+#' @param ... arguments to pass to [library()]
 #' @details
 #' used to unload and reload a package namespace,
 #' can be helpful when a package has been rebuilt in a
@@ -12,8 +12,9 @@ reload_namespace <- function(pkg, ...) {
 }
 
 
-#' @rdname reload_namespace
+#' reload internal package
+#' @param ... arguments to pass to [reload_namespace()]
 #' @export
-reload_internal <- function() {
-  reload_namespace("internal")
+reload_internal <- function(...) {
+  reload_namespace("internal", ...)
 }
